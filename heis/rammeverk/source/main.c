4a0e90331a800f5d112744c3246fe83ba9de4be1
nadiawangberg <nadiagw@stud.ntnu.no>
2019-03-26 10:55:20 +0100

    liftInit done

--- heis/rammeverk/source/main.c
+++ heis/rammeverk/source/main.c
@@ -1,4 +1,5 @@
 #include "elev.h"
+#include "liftMovement.c"
 #include <stdio.h>
 
 /**
@@ -13,7 +14,7 @@ int main() {
         printf("Unable to initialize elevator hardware!\n");
         return 1;
     }
-
+    /*
     printf("Press STOP button to stop elevator and exit program.\n");
 
     elev_set_motor_direction(DIRN_UP);
@@ -32,6 +33,10 @@ int main() {
             break;
         }
     }
+    */
+    //elev_set_motor_direction(DIRN_UP);
 
+    positionInit();
+    printf("\n%i\n",my_floor);
     return 0;
 }
