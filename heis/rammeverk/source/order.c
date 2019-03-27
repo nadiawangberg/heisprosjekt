5ffd5e34ebb9c36339775cd65d235a0d620f8d81
Ludvig <ludvigls@stud.ntnu.no>
2019-03-27 18:47:20 +0100

    change from one to two order lists, not done at all

--- heis/rammeverk/source/order.c
+++ heis/rammeverk/source/order.c
@@ -1,31 +1,94 @@
 #include "elev.c"
-void printOrders(){
+/*void printOrders(){
 	for (int i=0;i<4;i+=1){
 		for(int j=0;j<2;j+=1){
 			printf("%d ",order_priority[i][j])
 		}
 		printf("\n")
 	}
+}*/
+int maxValOfList(int* list, int length){
+	int max_val=list[0];
+	for(int i = 1;i<length;i+=1){
+		if(list[i]>max_val){
+			max_val=list[i]
+		}
+	}
 }
-void addOrder(Floor floor, elev_motor_direction_t dir){
+void addOrder(Floor floor, elev_motor_direction_t dir){ //updated to work with separete order lists
 	if(dir==0){
+		if (order_priority_in[floor]==0) {
+			order_priority_in[floor]=maxValOfList(order_priority_in,4)+1;	
+		
+	}
+	else if {
+		switch (floor){
+			case FIRST:
+				if (order_priority_out[0]==0){
+					order_priority_out[0]=maxValOfList(order_priority_out,6)+1;
+				}
+			case SECOND:
+				if(order_priority_out[1+dir==DIRN_UP]==0){
+					order_priority_out[1+dir==DIRN_UP]=maxValOfList(order_priority_out,6)+1;
+				}
+			case THIRD:
+				if(order_priority_out[3+dir==DIRN_UP]==0){
+					order_priority_out[3+dir==DIRN_UP]=maxValOfList(order_priority_out,6)+1;
+				}
+			case FOURTH:
+				if (order_priority_out[5]==0){
+					order_priority_out[5]=maxValOfList(order_priority_out,6)+1;
+				}
+		}
+		}
+	}
+	/*if(dir==0){
 		if(current_floor>floor){
 			order_priority[floor][0];
 		}
 		else{
 			order_priority[floor][1];
 		}
-	}
+	}                                        //old garbage 
 	else if(dir==DIRN_DOWN){
 			order_priority[floor][0];
 		}
 	else{
 			order_priority[floor][1];
 		}
+		*/
 }
 
 void removeOrder(Floor floor,elev_motor_direction_t dir){
-	elev_set_button_lamp(BUTTON_COMMAND,floor, 0);
+	
+	if(dir==0){
+		order_priority_in[floor]=0;	 //lights needs to be fixed
+	}
+	else if {
+		switch (floor){
+			case FIRST:
+					order_priority_out[0]=0;
+
+		
+			case SECOND:
+					order_priority_out[1+dir==DIRN_UP]=0;
+
+			case THIRD:
+					order_priority_out[3+dir==DIRN_UP]=0;
+
+			case FOURTH:
+					order_priority_out[5]=0;
+				
+		}
+		}
+	}
+
+
+
+
+
+
+	/*elev_set_button_lamp(BUTTON_COMMAND,floor, 0);
 	order_priority[floor][dir==DIRN_UP]
 	if(dir==DIRN_UP){
 		elev_set_button_lamp(BUTTON_UP,floor, 0);
@@ -34,7 +97,7 @@ void removeOrder(Floor floor,elev_motor_direction_t dir){
 	else{
 		elev_set_button_lamp(BUTTON_DOWN,floor, 0);
 		order_priority[floor][0]=0;
-	}
+	}*/
     /*
 	switch(floor):
 		case FIRST:{
@@ -71,7 +134,15 @@ void removeOrder(Floor floor,elev_motor_direction_t dir){
 
 	return
 }
-bool takeBonusOrder(){
+bool takeBonusOrder(){ //obv not done
+	if(order_priority_in[current_floor]>1){
+		removeOrder(floor,0);
+	}
+	if(current_floor>last_floor){
+
+		removeOrder(floor,DIRN_UP)
+	} 
+	/*
 	if (current_floor>last_floor){
 		if(order_priority[current_floor][1]>0){
 			order_priority[current_floor][1]=0;
@@ -82,6 +153,7 @@ bool takeBonusOrder(){
 			order_priority[current_floor][0]=0;
 		}
 	}
+	*/
 }
 bool takeOrder(){
 
--- heis/rammeverk/source/order.h
+++ heis/rammeverk/source/order.h
@@ -3,7 +3,10 @@
 	elev_motor_direction_t dir;
 	int priority;
 } Order;"""
-int order_priority[4][2]={{0,0},{0,0},{0,0},{0,0}};
+//int order_priority_in[4][2]={{0,0},{0,0},{0,0},{0,0}};
+//int order_priority_out[4][2]={{0,0},{0,0},{0,0},{0,0}};
+int order_priority_in[4]={0,0,0,0}
+int order_priority_out[6]={0,0,0,0,0,0}
 void addOrder(Floor floor,elev_motor_direction_t dir);
 void removeOrder(Floor floor);
 bool takeBonusOrder();
