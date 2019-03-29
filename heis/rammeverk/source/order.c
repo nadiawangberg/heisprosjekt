e9508eccc049af9d04ef0190d77a2b9665c66413
Ludvig <ludvigls@stud.ntnu.no>
2019-03-29 15:02:19 +0100

    simple elev algorithm

--- heis/rammeverk/source/order.c
+++ heis/rammeverk/source/order.c
@@ -1,91 +1,78 @@
 #include "elev.c"
 void printOrders(){
+	printf("order_priority_up: ")
 	for (int i=0;i<4;i+=1){
-		for(int j=0;j<2;j+=1){
-			printf("%d ",order_priority[i][j])
-		}
-		printf("\n")
+			printf("%d ",order_priority_up[i])
+	}
+	printf("\n order_priority_down: ")
+	for (int i=0;i<4;i+=1){
+			printf("%d ",order_priority_down[i])
 	}
 }
+
+/*int maxValOfList(int* list, int length){
+	int max_val=list[0];
+	for(int i = 1;i<length;i+=1){
+		if(list[i]>max_val){
+			max_val=list[i]
+		}
+	}
+}*/
+
+//Nadia suggestion, make a new struct NOT use elev_motor_direction_t for this
+
+
 void addOrder(Floor floor, elev_motor_direction_t dir){
-	if(dir==0){
-		if(current_floor>floor){
-			order_priority[floor][0];
-		}
-		else{
-			order_priority[floor][1];
-		}
+	if(dir==DIRN_UP){ // person wanting to go up
+		order_priority_up[floor]=1;
+		elev_set_button_lamp(BUTTON_UP,floor, 1);	
 	}
 	else if(dir==DIRN_DOWN){
-			order_priority[floor][0];
-		}
-	else{
-			order_priority[floor][1];
-		}
-}
-
-void removeOrder(Floor floor,elev_motor_direction_t dir){
-	elev_set_button_lamp(BUTTON_COMMAND,floor, 0);
-	order_priority[floor][dir==DIRN_UP]
-	if(dir==DIRN_UP){
-		elev_set_button_lamp(BUTTON_UP,floor, 0);
-		order_priority[floor][1]=0;
+		order_priority_down[floor]=1;
+		elev_set_button_lamp(BUTTON_DOWN,floor, 1);
 	}
-	else{
+	else{ // aka dir == DIRN_STOP, aka person in lift pressed button (1-4)
+		elev_set_button_lamp(BUTTON_COMMAND,floor, 1);
+		if(DIRECTION==DIRN_UP){
+			order_priority_up[floor]=1;
+		}
+		else{ // Dir = down
+			order_priority_down[floor]=1;
+		}
+	}
+}
+	
+
+//hjelpe funksjon
+	
+// called in each floor, no matter what (prob 2 times for 1. and 4. floor)
+bool removeOrder(){ //obv NOT done
+	if(DIRECTION==DIRN_UP){
+		if(order_priority_up[floor]==0)
+			order_priority_up[floor]=0;
+			elev_set_button_lamp(BUTTON_UP,floor, 0);
+	}
+	else if(order_priority_down[floor]==0){
+		order_priority_down[floor]=0;
 		elev_set_button_lamp(BUTTON_DOWN,floor, 0);
-		order_priority[floor][0]=0;
-	}
-    /*
-	switch(floor):
-		case FIRST:{
-			order_priority[0]=0;
-			elev_set_button_lamp(BUTTON_CALL_UP,floor, 0);
-			break;
-		}
-		case SECOND:{
-			if (dir==DIRN_DOWN){
-				order_priority[1]=0;
-			}
-			else{
-				elev_set_button_lamp(BUTTON_CALL_UP,floor, 0);
-				order_priority[2]=0;
-			}
-			break;
-		}
-		case THIRD:{
-			if (dir==DIRN_DOWN){
-				order_priority[3]=0;
-			}
-			else{
-				order_priority[4]=0;
-				elev_set_button_lamp(BUTTON_CALL_UP,floor, 0);
-			}
-			break;
-		}
-		case FOURTH:{
-			order_priority[5]=0;
-			elev_set_button_lamp(BUTTON_CALL_UP,floor, 0);
-			break;
-		}
-		*/
+	} 
+	elev_set_button_lamp(BUTTON_COMMAND,floor, 0);
+}
 
-	return
-}
-bool takeBonusOrder(){
-	if (current_floor>last_floor){
-		if(order_priority[current_floor][1]>0){
-			order_priority[current_floor][1]=0;
-		}
-	}
-	else{
-		if(order_priority[current_floor][0]>0){
-			order_priority[current_floor][0]=0;
-		}
-	}
-}
-bool takeOrder(){
 
+void checkForOrders(){//feels if we have orders (button press), adds them to queue if there are orders
+	for(int i=0;i<4;i+=1){
+		if(elev_get_button_signal(BUTTON_CALL_UP,i)){
+			addOrder(DIRN_UP,i);
+		}
+		if(elev_get_button_signal(BUTTON_COMMAND,i)){
+			addOrder(0,i);
+		}
+		if(elev_get_button_signal(BUTTON_CALL_DOWN,i)){
+			addOrder(DIRN_DOWN,i);
+		}
+	}
 }
-void checkForOrderAndAddOrderToList(){
+void (){
 
 }
--- heis/rammeverk/source/order.h
+++ heis/rammeverk/source/order.h
@@ -1,12 +1,37 @@
 #include "liftMovement.h"
-"""typedef Struct tag_order {
+
+/*
+typedef Struct tag_order {
 	elev_motor_direction_t dir;
 	int priority;
-} Order;"""
-int order_priority[4][2]={{0,0},{0,0},{0,0},{0,0}};
-void addOrder(Floor floor,elev_motor_direction_t dir);
-void removeOrder(Floor floor);
-bool takeBonusOrder();
-bool takeOrder();
-void checkForOrderAndAddOrderToList();
+} Order;
+*/
+
+//int order_priority_in[4][2]={{0,0},{0,0},{0,0},{0,0}};
+//int order_priority_out[4][2]={{0,0},{0,0},{0,0},{0,0}};
+
+// Not global, member variables for order
+bool order_priority_up[4]={0,0,0,0};
+bool order_priority_down[4]={0,0,0,0};
+
+// probably global?
+elev_motor_direction_t DIRECTION;
+
+
+void addOrder(Floor floor, elev_motor_direction_t dir); // dir = order_direction
+void removeOrder(Floor floor); //removes a single order
+ //removes all orders in the right direction in the current floor
+void checkForOrders(); 
 void printOrders();
+
+//vent med
+bool takeOrder(); // maskineri, bevegelse osv, burde dette være i denne modulen??
+
+
+
+//idea #3: why not drop priority, it sucks either way.  
+
+/* new idea, may suck. Have a number of orders above current floor. 
+When we check to stop we just check the ligths lmao. 
+The number of lights that are on are the amount of orders that.
+*/
